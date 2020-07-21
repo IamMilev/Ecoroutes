18d0363de0770ecff6e33ad11f69bfeceff5c93b
IamMilev <mileffdesign@gmail.com>
2020-07-21 18:35:26 +0300

    first commit

---
+++ backend/models/ecoroute.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ecorouteSchema = new Schema({
+    username: { type: String, required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    duration: { type: Number, required: true },
+    date: { type: Date, required: true },
+}, {
+    timestamps: true,
+});
+
+const Ecoroute = mongoose.model('Ecoroute', ecorouteSchema);
+
+module.exports = Ecoroute;
