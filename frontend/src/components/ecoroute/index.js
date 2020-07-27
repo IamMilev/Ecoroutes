dfeecb9aa7f04a5d0d763a41cbe376db56ff8590
IamMilev <mileffdesign@gmail.com>
2020-07-27 18:17:49 +0300

    Adding stuff

---
+++ frontend/src/components/ecoroute/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+// import styles from './index.module.css'
+
+const Ecoroute = ({ title }) => {
+    return (
+        <div>
+            {title}
+        </div>
+    )
+}
+
+export default Ecoroute
