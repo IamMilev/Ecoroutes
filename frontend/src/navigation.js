33619db7129d9a0c46f01ead73b14ec8bed6372a
IamMilev <mileffdesign@gmail.com>
2020-08-03 16:19:16 +0300

    Login/register/

--- frontend/src/navigation.js
+++ frontend/src/navigation.js
@@ -6,6 +6,8 @@ import {
 } from 'react-router-dom'
 import EcoroutesListPage from "./pages/ecoroutes-list/index"
 import AddEcoroutePage from "./pages/add-ecoroute/index";
+import SignInPage from "./pages/sign-in";
+import SignUpPage from "./pages/sign-up";
 
 const Navigation = () => {
     return(
@@ -13,6 +15,8 @@ const Navigation = () => {
             <Switch>
                 <Route path="/ecoroutes" component={EcoroutesListPage} />
                 <Route path="/addecoroute" component={AddEcoroutePage} />
+                <Route path="/signin" component={SignInPage} />
+                <Route path="/signup" component={SignUpPage} />
             </Switch>
         </BrowserRouter>
     )
--- frontend/src/pages/sign-in/index.js
+++ frontend/src/pages/sign-in/index.js
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, {Component} from 'react'
+import Title from "../../components/title";
+import SubmitButton from "../../components/button/submit-button";
+import PageLayout from "../../components/page-layout";
+import Input from "../../components/input";
 
-const SignInPage = () => {
-    return(
-        <>
-        </>
-    )
+class SignInPage extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            email: "",
+            password: ""
+        }
+    }
+
+    onChange = (event, type) => {
+        const newState = {}
+        newState[type] = event.target.value
+
+        this.setState(newState)
+    }
+
+    render() {
+        const {
+            email,
+            password
+        } = this.state
+
+        return (
+            <PageLayout>
+                <Title title="Sign In"/>
+                <Input
+                    value={email}
+                    onChange={(e) =>  this.onChange(e, 'email')}
+                    label="Email"
+                    id="email"
+                />
+                <Input
+                    value={password}
+                    onChange={(e) =>  this.onChange(e, 'password')}
+                    label="Password"
+                    id="password"
+                />
+                <SubmitButton title="Sign In"/>
+            </PageLayout>
+        )
+    }
 }
 
 export default SignInPage
